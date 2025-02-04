a30e73c2bff19bc4028db0e846e747ae1cdd904a
Gabriel Machado <gabriel.delmachado@gmail.com>
2025-02-03 22:00:37 -0300

    performance increase

--- packages/front/src/pages/logview/HighlitableText.vue
+++ packages/front/src/pages/logview/HighlitableText.vue
@@ -1,26 +1,44 @@
-<template>
-  <div v-html="messageHTML(value)"></div>
-</template>
+<script lang="ts">
+import { defineComponent, h, VNode } from 'vue';
 
-<script lang="ts" setup>
-const props = defineProps<{
-  value: string;
-  search?: string;
-}>();
-
-function messageHTML(message: string) {
-  if (!props.search) {
-    return message;
+function messageHTML(message: string, search?: string) {
+  if (!search) {
+    return h('div', message);
   }
-  const rgx = new RegExp(props.search, 'ig');
-  return message.replaceAll(rgx, (match) => {
-    return `<span class="log-view-hightlight">${match}</span>`;
-  });
+  const rgx = new RegExp(search, 'ig');
+
+  const textNodes: VNode[] = [];
+  let currentIndex = 0;
+  for (const match of message.matchAll(rgx)) {
+    const str = match[0];
+    const index = match.index;
+    textNodes.push(h('span', message.slice(currentIndex, index)));
+    textNodes.push(h('span', { class: 'log-view-highlight' }, str));
+    currentIndex = index + str.length;
+  }
+  textNodes.push(h('span', message.slice(currentIndex)));
+  return h('div', textNodes);
 }
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    search: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props) {
+    return () => messageHTML(props.value, props.search);
+  },
+});
 </script>
 
 <style>
-.log-view-hightlight {
+.log-view-highlight {
   @apply bg-prime-200 rounded-sm;
 }
 </style>
