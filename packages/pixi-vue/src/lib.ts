8a4f3745410bdb895c3867dd94237f78da25db8b
Gabriel Machado <gabriel.delmachado@gmail.com>
2025-03-06 01:06:37 -0300

    fixed build issues

--- packages/pixi-vue/src/lib.ts
+++ packages/pixi-vue/src/lib.ts
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { GElement } from "#els/Elements.ts";
-export { GElement };
 
 export function startApp(el: HTMLElement): () => void {
   const app = createApp(App);
