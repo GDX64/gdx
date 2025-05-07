707a087bae69b071d4d95ebfd127b46067aa2a9c
glmachado <glmachado@nelogica.com.br>
2025-05-07 16:06:11 -0300

    useScale composable

--- packages/pixi-vue/src/components/BarChart.vue
+++ packages/pixi-vue/src/components/BarChart.vue
@@ -1,11 +1,11 @@
 <template>
   <g-raw
     fill="white"
-    :drawFunction="drawFn"
     :border="1"
     borderColor="#000000"
     :padding="30"
     @layoutupdate="onLayoutUpdate"
+    :drawFunction="drawScale"
   >
     <g-rect
       height="100%"
@@ -35,7 +35,8 @@
 import { Align, FlexDirection, GRaw, GRect } from "#els/appRenderers.ts";
 import { ElementInterface } from "#els/renderTypes.ts";
 import { computed, reactive, watchEffect } from "vue";
-import { scaleLinear, interpolateReds } from "d3";
+import { interpolateReds } from "d3";
+import { useScales } from "./scale/scaleComposable";
 
 const hovered = reactive(new Set<number>());
 
@@ -69,7 +70,17 @@ const limits = computed(() => {
   return { minY, maxY, minX, maxX };
 });
 
-const { scales, drawTicks } = useScales(() => limits.value);
+const { scales, drawScale } = useScales(
+  () => limits.value,
+  () => {
+    return {
+      minX: paddingX.value,
+      maxX: size.width - paddingX.value,
+      minY: paddingY,
+      maxY: size.height - paddingY,
+    };
+  }
+);
 
 const bars = computed(() => {
   return barsRaw.map((bar) => {
@@ -86,39 +97,9 @@ const size = reactive({
   height: 500,
 });
 
-type ScaleLimits = {
-  minY: number;
-  maxY: number;
-  minX: number;
-  maxX: number;
-};
-
 function onLayoutUpdate(element: ElementInterface) {
   size.width = element.getWidth();
   size.height = element.getHeight();
-}
-
-function drawFn(ctx: CanvasRenderingContext2D, element: ElementInterface) {
-  const { scaleX, scaleY } = scales.value;
-  const [minX, maxX] = scaleX.domain();
-  const [minY, maxY] = scaleY.domain();
-  ctx.beginPath();
-  ctx.fillStyle = "#ffffff";
-  ctx.strokeStyle = "#000000";
-  ctx.lineWidth = 1;
-  ctx.beginPath();
-  ctx.rect(0, 0, element.getWidth(), element.getHeight());
-  ctx.fill();
-  ctx.beginPath();
-  ctx.rect(0.5, 0.5, element.getWidth() - 1, element.getHeight() - 1);
-  ctx.stroke();
-  ctx.moveTo(scaleX(0), scaleY(0));
-  ctx.lineTo(scaleX(0), scaleY(maxY));
-  ctx.moveTo(scaleX(0), scaleY(0));
-  ctx.lineTo(scaleX(maxX), scaleY(0));
-  ctx.stroke();
-
-  drawTicks(ctx);
 }
 
 function barDrawFn(ctx: CanvasRenderingContext2D, element: ElementInterface) {
@@ -136,53 +117,5 @@ function barDrawFn(ctx: CanvasRenderingContext2D, element: ElementInterface) {
     element.getWidth() / 2,
     element.getHeight() + 10
   );
-}
-
-function useScales(limits: () => ScaleLimits) {
-  const scales = computed(() => {
-    const { minY, maxY, minX, maxX } = limits();
-    const scaleX = scaleLinear()
-      .domain([minX, maxX])
-      .range([paddingX.value, size.width])
-      .nice(10);
-    const scaleY = scaleLinear()
-      .domain([minY, maxY])
-      .range([size.height - paddingY, paddingY])
-      .nice(10);
-    return { scaleX, scaleY };
-  });
-
-  function drawTicks(ctx: CanvasRenderingContext2D) {
-    ctx.save();
-    const { scaleX, scaleY } = scales.value;
-    const [_, maxX] = scaleX.range();
-    const ticks = scaleY.ticks();
-    ticks.forEach((tickValue) => {
-      const y = scaleY(tickValue);
-      ctx.beginPath();
-      ctx.strokeStyle = "black";
-      const x0 = scaleX(0);
-      ctx.moveTo(x0 - 5, y);
-      ctx.lineTo(x0 + 5, y);
-      ctx.stroke();
-      ctx.beginPath();
-      if (tickValue !== 0) {
-        ctx.save();
-        ctx.moveTo(x0 + 5, y);
-        ctx.setLineDash([5, 5]);
-        ctx.lineTo(scaleX(maxX), y);
-        ctx.strokeStyle = "#d7d7d7";
-        ctx.stroke();
-        ctx.restore();
-      }
-      ctx.fillStyle = "black";
-      ctx.textBaseline = "middle";
-      ctx.textAlign = "right";
-      ctx.fillText(tickValue.toString(), x0 - 8, y);
-    });
-    ctx.restore();
-  }
-
-  return { scales, drawTicks };
 }
 </script>
---
+++ packages/pixi-vue/src/components/scale/scaleComposable.ts
@@ -0,0 +1,85 @@
+import { computed } from "vue";
+import { scaleLinear } from "d3";
+import { ElementInterface } from "#els/renderTypes.ts";
+
+type ScaleLimits = {
+  minY: number;
+  maxY: number;
+  minX: number;
+  maxX: number;
+};
+
+export function useScales(domain: () => ScaleLimits, image: () => ScaleLimits) {
+  const scales = computed(() => {
+    const { minY, maxY, minX, maxX } = domain();
+    const imageLimits = image();
+
+    const scaleX = scaleLinear()
+      .domain([minX, maxX])
+      .range([imageLimits.minX, imageLimits.maxX])
+      .nice(10);
+
+    const scaleY = scaleLinear()
+      .domain([minY, maxY])
+      .range([imageLimits.maxY, imageLimits.minY])
+      .nice(10);
+
+    return { scaleX, scaleY };
+  });
+
+  function drawScale(ctx: CanvasRenderingContext2D, element: ElementInterface) {
+    const { scaleX, scaleY } = scales.value;
+    const [minX, maxX] = scaleX.domain();
+    const [minY, maxY] = scaleY.domain();
+    ctx.beginPath();
+    ctx.fillStyle = "#ffffff";
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.rect(0, 0, element.getWidth(), element.getHeight());
+    ctx.fill();
+    ctx.beginPath();
+    ctx.rect(0.5, 0.5, element.getWidth() - 1, element.getHeight() - 1);
+    ctx.stroke();
+    ctx.moveTo(scaleX(0), scaleY(0));
+    ctx.lineTo(scaleX(0), scaleY(maxY));
+    ctx.moveTo(scaleX(0), scaleY(0));
+    ctx.lineTo(scaleX(maxX), scaleY(0));
+    ctx.stroke();
+
+    drawTicks(ctx);
+  }
+
+  function drawTicks(ctx: CanvasRenderingContext2D) {
+    ctx.save();
+    const { scaleX, scaleY } = scales.value;
+    const [_, maxX] = scaleX.domain();
+    const ticks = scaleY.ticks();
+    ticks.forEach((tickValue) => {
+      const y = scaleY(tickValue);
+      ctx.beginPath();
+      ctx.strokeStyle = "black";
+      const x0 = scaleX(0);
+      ctx.moveTo(x0 - 5, y);
+      ctx.lineTo(x0 + 5, y);
+      ctx.stroke();
+      ctx.beginPath();
+      if (tickValue !== 0) {
+        ctx.save();
+        ctx.moveTo(x0 + 5, y);
+        ctx.setLineDash([5, 5]);
+        ctx.lineTo(scaleX(maxX), y);
+        ctx.strokeStyle = "#d7d7d7";
+        ctx.stroke();
+        ctx.restore();
+      }
+      ctx.fillStyle = "black";
+      ctx.textBaseline = "middle";
+      ctx.textAlign = "right";
+      ctx.fillText(tickValue.toString(), x0 - 8, y);
+    });
+    ctx.restore();
+  }
+
+  return { scales, drawScale };
+}
