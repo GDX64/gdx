de5fe39b053c4e336654f8c7d50755edc6ab16cd
Gabriel Machado <gabriel.delmachado@gmail.com>
2025-10-25 15:30:32 -0300

    better interface

--- packages/front/src/pages/codecs/codecs.test.ts
+++ packages/front/src/pages/codecs/codecs.test.ts
@@ -1,59 +1,46 @@
 describe('Codecs', () => {
   test('should run tests', () => {
-    const codec = new CodecBuilder().addInt('foo').addInt('bar');
+    const nested = new CodecBuilder().add('a', Int).add('b', Int);
+    const arrOfInts = new ArraySerializable(Int);
+    const arrOfArrOfInts = new ArraySerializable(arrOfInts);
+    const arrOfCodecs = new ArraySerializable(new CodecBuilder().add('hello', Int));
+    const codec = new CodecBuilder()
+      .add('foo', Int)
+      .add('bar', Int)
+      .add('nested', nested)
+      .add('arrOfInts', arrOfInts)
+      .add('arrOfArrOfInts', arrOfArrOfInts)
+      .add('arrOfCodecs', arrOfCodecs);
 
-    const arrayBuilder = new CodecBuilder().addInt('item1').addInt('item2');
-    const flatArrayBuilder = new CodecBuilder().addInt(null);
+    console.log(codec.generateEncoderCode());
+    // console.log(codec.generateDecoderCode());
 
-    const otherCodec = new CodecBuilder()
-      .addInt('baz')
-      .addCodec('nested', codec)
-      .addArray('arr', arrayBuilder)
-      .addArray('flatArr', flatArrayBuilder);
-    console.log(otherCodec.generateDecoder());
-    console.log(otherCodec.generateEncoder());
-
-    const encoderFn = otherCodec.createEncoderFunction();
+    const encoderFn = codec.createEncoderFunction();
     const objectToEncode = {
-      baz: 42,
-      nested: { foo: 1, bar: 2 },
-      arr: [{ item1: 1, item2: 2 }],
-      flatArr: [7, 8, 9],
+      foo: 42,
+      bar: 42,
+      nested: { a: 1, b: 2 },
+      arrOfInts: [10, 20, 30],
+      arrOfArrOfInts: [
+        [1, 2],
+        [3, 4, 5],
+      ],
+      arrOfCodecs: [{ hello: 100 }, { hello: 200 }],
     };
     const encoder = CodecBuilder.createEncoder();
     encoderFn(encoder, objectToEncode);
     const buffer = encoder.getBuffer();
     console.log('Encoded Buffer:', buffer);
-    const decoderFn = otherCodec.createDecoderFunction();
+    const decoderFn = codec.createDecoderFunction();
     const decoder = CodecBuilder.createDecoder(buffer);
     const decodedObject = decoderFn(decoder);
     expect(decodedObject).toEqual(objectToEncode);
   });
 });
 
-enum Tags {
-  INT,
-  CODEC,
-  ARRAY,
-}
+type Field = { name: string; serializable: Serializable };
 
-type Field =
-  | {
-      type: Tags.INT;
-      name: string | null;
-    }
-  | {
-      type: Tags.CODEC;
-      name: string;
-      codec: CodecBuilder;
-    }
-  | {
-      type: Tags.ARRAY;
-      name: string;
-      codec: CodecBuilder;
-    };
-
-class CodecBuilder {
+class CodecBuilder implements Serializable {
   private fields: Field[] = [];
 
   static createEncoder() {
@@ -65,96 +52,92 @@ class CodecBuilder {
   }
 
   createEncoderFunction() {
-    return eval(this.generateEncoder());
+    return eval(this.generateEncoderCode());
   }
 
   createDecoderFunction() {
-    return eval(this.generateDecoder());
+    return eval(this.generateDecoderCode());
   }
 
-  addInt(name: string | null) {
-    this.fields.push({ type: Tags.INT, name });
+  add(name: string, serializable: Serializable) {
+    this.fields.push({ name, serializable });
     return this;
   }
 
-  addArray(name: string, codec: CodecBuilder) {
-    this.fields.push({ type: Tags.ARRAY, name, codec });
-    return this;
-  }
-
-  addCodec(name: string, codec: CodecBuilder) {
-    this.fields.push({ type: Tags.CODEC, name, codec });
-    return this;
-  }
-
-  generateEncoder() {
+  generateEncoderCode() {
     const lines: string[] = [];
     lines.push(`(encoder, obj)=>{`);
     this.fields.map((field) => {
-      if (field.type === Tags.INT) {
-        if (field.name == null) {
-          lines.push(`encoder.int(obj);`);
-        } else {
-          lines.push(`encoder.int(obj.${field.name});`);
-        }
-      } else if (field.type === Tags.CODEC) {
-        lines.push(`(${field.codec.generateEncoder()})(encoder, obj.${field.name});`);
-      } else if (field.type === Tags.ARRAY) {
-        //encode the array length first
-        lines.push(`encoder.int(obj.${field.name}.length);`);
-        lines.push(`const itemEncoder_${field.name} = ${field.codec.generateEncoder()};`);
-        lines.push(`obj.${field.name}.forEach((item)=>{`);
-        lines.push(`itemEncoder_${field.name}(encoder, item);`);
-        lines.push(`});`);
-      }
+      lines.push(`${field.serializable.encoder(`obj.${field.name}`)};`);
     });
     lines.push(`}`);
     return lines.join('\n');
   }
 
-  generateDecoder() {
+  generateDecoderCode() {
     const lines: string[] = [];
     lines.push(`(decoder)=>{`);
     lines.push(`let obj = {};`);
     this.fields.map((field) => {
-      if (field.type === Tags.INT) {
-        if (field.name == null) {
-          lines.push(`obj = decoder.int();`);
-        } else {
-          lines.push(`obj.${field.name} = decoder.int();`);
-        }
-      } else if (field.type === Tags.CODEC) {
-        lines.push(`obj.${field.name} = (${field.codec.generateDecoder()})(decoder);`);
-      } else if (field.type === Tags.ARRAY) {
-        lines.push(`obj.${field.name} = [];`);
-        lines.push(`const itemDecoder_${field.name} = ${field.codec.generateDecoder()};`);
-        lines.push(`const length_${field.name} = decoder.int();`);
-        lines.push(`for(let i=0; i<length_${field.name}; i++){`);
-        lines.push(`obj.${field.name}.push(itemDecoder_${field.name}(decoder));`);
-        lines.push(`}`);
-      }
+      lines.push(`obj.${field.name} = ${field.serializable.decoder()};`);
     });
     lines.push(`return obj;`);
     lines.push(`}`);
 
     return lines.join('\n');
   }
+
+  encoder(what: string): string {
+    const encoderFunction = this.generateEncoderCode();
+    return `(${encoderFunction})(encoder, ${what})`;
+  }
+
+  decoder(): string {
+    const decoderFunction = this.generateDecoderCode();
+    return `(${decoderFunction})(decoder)`;
+  }
 }
 
 interface Serializable {
-  encoder(): string;
+  encoder(what: string): string;
   decoder(): string;
 }
 
 class IntSerializable implements Serializable {
-  encoder() {
-    return `encoder.int(value);`;
+  encoder(what: string) {
+    return `encoder.int(${what})`;
   }
 
   decoder() {
-    return `decoder.int();`;
+    return `decoder.int()`;
   }
 }
+
+class ArraySerializable implements Serializable {
+  constructor(private itemSerializable: Serializable) {}
+
+  encoder(what: string) {
+    return `(()=>{encoder.int(${what}.length);
+      const arr = ${what};
+      arr.forEach((item)=>{
+        ${this.itemSerializable.encoder('item')};
+      })
+    })()`;
+  }
+
+  decoder(): string {
+    return `((decoder)=>{
+    const arr = [];
+    const length = decoder.int();
+    for(let i=0; i<length; i++){
+      arr.push(${this.itemSerializable.decoder()});
+    }
+    return arr;
+  })(decoder)`;
+  }
+}
+
+const Int = new IntSerializable();
 
 class Encoder {
   private buffer: number[] = [];
