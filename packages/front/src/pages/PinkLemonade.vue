01f0ca99b76f8dd2c17ba669cf0c1782af000429
glmachado <glmachado@nelogica.com.br>
2026-05-14 15:28:39 -0300

    fixed canvas

--- packages/front/src/pages/PinkLemonade.vue
+++ packages/front/src/pages/PinkLemonade.vue
@@ -6,12 +6,6 @@ import { onUnmounted } from 'vue';
 
 const ctx = example();
 
-const canvas = document.querySelector('canvas')!;
-canvas.style.position = 'fixed';
-canvas.style.top = '0';
-canvas.style.left = '0';
-canvas.style.zIndex = '10';
-
 onUnmounted(() => {
   ctx.destroy();
 });
